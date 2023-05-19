415438ee89c262371241a74560be62ebcf88383a
Minyongggg <naminyong97@gmail.com>
2023-05-19 11:01:38 +0900

    Spring 입문 강의 실습

---
+++ tutorial/hello-spring/src/main/java/hellospring/hellospring/SpringConfig.java
@@ -0,0 +1,32 @@
+package hellospring.hellospring;
+
+import hellospring.hellospring.repository.JdbcMemberRepository;
+import hellospring.hellospring.repository.MemberRepository;
+import hellospring.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+    }
+}
---
+++ tutorial/hello-spring/src/main/java/hellospring/hellospring/controller/HomeController.java
@@ -0,0 +1,14 @@
+package hellospring.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String HOme() {
+        return "home";
+    }
+
+}
