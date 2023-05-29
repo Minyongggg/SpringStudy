757e0d9e025724e5250bcc23d6a07a2286a8f581
Minyongggg <naminyong97@gmail.com>
2023-05-30 03:24:21 +0900

    Spring JPA 기초 강의 실습(2)

---
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import javax.persistence.*;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+
+        emf.close();
+    }
+}
